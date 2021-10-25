31adc78509980b1b63559b2c41c4cd4e9136330c
bkeyunknown <kikizan01@gmail.com>
2021-10-25 20:57:30 +0700

    initial Commit

---
+++ src/main/java/com/app/kuliah/dataMahasiswa/DataMahasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.app.kuliah.dataMahasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMahasiswaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/kuliah/dataMahasiswa/DataMahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kuliah.dataMahasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
